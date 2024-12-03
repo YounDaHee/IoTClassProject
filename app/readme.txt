69f36f35de5a6ce8375b24e573c901989ac07ec7
YounDaHee <dbsekgml518@gmail.com>
2024-12-03 13:53:35 +0900

    save log data

--- app/readme.txt
+++ app/readme.txt
@@ -1,5 +1,16 @@
-코드 위치는 app/java/com/example/iotprojectapplication/MainActivity.kt
-
-만약 실행에 문제가 생겼다면 build.gradle.kts(:app)에 문제가 있을 가능성이 높음
-compileSdk = 34과 targetSdk = 34, minSdk = 28
-SDK version 정보를 지금 실행 시키는 가상환경/휴대폰 OS 버전에 맞게 수정
+50초 동안 기록
+기록한 txt 파일은 json 형식으로 핸드폰 내의 Documents/iotSensingDataSave에 저장
+[
+    [
+        {
+            "SSID": "eduroam",
+            "MAC": "94:64:24:9f:69:e1",
+            "RSSI": -44
+        },
+        {
+            "SSID": "Smart-CAU",
+            "MAC": "94:64:24:9f:69:e0",
+            "RSSI": -44
+        },...
+    ]
+]
--- app/src/main/java/com/example/iotprojectapplication/MainActivity.kt
+++ app/src/main/java/com/example/iotprojectapplication/MainActivity.kt
@@ -3,32 +3,43 @@ package com.example.iotprojectapplication
 import android.Manifest
 import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.net.wifi.WifiManager
-import android.net.wifi.rtt.RangingRequest
-import android.net.wifi.rtt.RangingResult
-import android.net.wifi.rtt.RangingResultCallback
-import android.net.wifi.rtt.WifiRttManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.os.Environment
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.core.app.ActivityCompat
-import java.util.concurrent.Executors
+import java.io.File
+import java.io.FileWriter
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     private lateinit var wifiManager: WifiManager
     private lateinit var wifiReceiver: BroadcastReceiver // 리시버 선언
     private val handler = Handler(Looper.getMainLooper())
 
+    // 데이터 저장 위치 설정
+    val folderName = "iotSensingDataSave"
+    val customDir = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), folderName)
+
     // class init 메서드와 유사. 처음 페이지 생성 시에 실행
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // 데이터 저장 폴더 생성
+        if (!customDir.exists()) {
+            val isCreated = customDir.mkdirs()
+            if (!isCreated) {
+                println("Failed to create directory!")
+                return
+            }
+        }
         wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
     }
 
@@ -36,44 +47,89 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
-        var count = 0 // 화면이 갱신 되고 있음을 알려줌(이후 데이터 수집의 counting 변수로 사용 예정)
-        val apText: TextView = findViewById(R.id.check)// 화면에 보여주는 Text 객체 선언
+        val sampelingRate : Long = 500 // 데이터 모으는 시간 간격
+        val countSet = 100 // 데이터 모으는 수 셋팅
+        // sampleing rate 500ms*100 = 50초동안 데이터 수집
+
+        var count = 0
+
+        lateinit var file :File // 센싱 데이터 저장을 위해
+        lateinit var fileWriter: FileWriter
+
+        val regiButton: Button = findViewById(R.id.registration)// 등록 버튼 객체 선언
+        val dataName : EditText = findViewById(R.id.dataName)// 위치 이름 등록
+        val countNum: TextView = findViewById(R.id.CountNum)// 카운팅
+        val apNum: TextView = findViewById(R.id.TotalNum)// 공유기 수
+        val apText: TextView = findViewById(R.id.allDataSet)// 현재 확인 가능한 공유기 데이터
+
 
         // RSSI 데이터 수집하는 메서드
         val updateWifiInfo = object : Runnable {
-            override fun run() {
-                // Permission 여부 확인
-                if (ActivityCompat.checkSelfPermission(
-                        this@MainActivity,
-                        Manifest.permission.ACCESS_FINE_LOCATION
-                    ) == PackageManager.PERMISSION_GRANTED
-                ) {
-                    // Wi-Fi 스캔 결과 가져오기
-                    wifiManager.startScan()
-                    var apLists = wifiManager.scanResults
 
-                    // String 변수에 각 데이터들 저장
-                    var checkAP = "${count++}: ${apLists.size}\n"
-                    for (ap in apLists) {
-                        checkAP += "[${ap.SSID}] [${ap.BSSID}] ${ap.level}\n"
+            override fun run() {
+                try {
+                    // Permission 여부 확인
+                    if (ActivityCompat.checkSelfPermission(
+                            this@MainActivity,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        // Wi-Fi 스캔 결과 가져오기
+                        wifiManager.startScan()
+                        var apLists = (wifiManager.scanResults).sortedByDescending { it.level }
+
+                        // String 변수에 각 데이터들 저장
+                        countNum.text = (--count).toString()
+                        apNum.text = apLists.size.toString()
+                        var checkAP = ""
+                        fileWriter.append("[")
+                        val lastIndex = apLists.size-1
+                        for ((index, ap) in apLists.withIndex()) {
+                            checkAP += "[${ap.SSID}] [${ap.BSSID}] ${ap.level}\n"
+                            fileWriter.append("{\"SSID\": \"${ap.SSID}\", \"MAC\":\"${ap.BSSID}\", \"RSSI\":${ap.level}}")
+
+                            if (index != lastIndex) {
+                                fileWriter.append(",")
+                            }
+                        }
+
+                        apText.text = checkAP // TextView 업데이트
+                    } else {
+                        // Permission에 문제 있음
+                        apText.text = "Location permission required!"
+                        return
                     }
 
-                    apText.text = checkAP // TextView 업데이트
-                } else {
-                    // Permission에 문제 있음
-                    apText.text = "Location permission required!"
+                    //out of range 예방(데이터 모집 시간 연산에 이용할 예정)
+                    if (count > 0) {
+                        //sampelingRate 밀리 초 마다 반복 하도록
+                        fileWriter.append("],\n")
+                        handler.postDelayed(this, sampelingRate)
+                    } else {
+                        fileWriter.append("]]")
+                        fileWriter.flush()
+                        fileWriter.close()
+                        regiButton.isEnabled = true
+                    }
+                }catch(e: IOException){
+                    //파일을 열고, 닫는 과정에서 문제가 발생함
+                    apText.text = e.toString()
                 }
-
-                //out of range 예방(데이터 모집 시간 연산에 이용할 예정)
-                if(count == 100) count = 0;
-
-                //500ms 마다 반복 하도록
-                handler.postDelayed(this, 500)
             }
+
         }
 
-        // 첫 수행
-        handler.post(updateWifiInfo)
+        regiButton.setOnClickListener {
+            count = countSet
+            if(dataName.text.toString() != ""){
+                regiButton.isEnabled = false
+                file = File(customDir, dataName.text.toString()+".txt")
+                fileWriter  = FileWriter(file, false)
+                fileWriter.append("[")
+                handler.post(updateWifiInfo)
+            }
+
+        }
     }
 
     // 페이지 종료 시 호출
